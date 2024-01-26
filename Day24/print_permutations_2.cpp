0be9fef988b5018f409f9deee191402d9cf202ae
Shahi Shreshth <54995727+shahishree03@users.noreply.github.com>
2024-01-26 16:49:26 +0530

    Create print_permutations_2.cpp

---
+++ Day24/print_permutations_2.cpp
@@ -0,0 +1,19 @@
+class Sol{
+private:
+void recurr(int index, vector<int>& nums, vector<vector<int>>& ans)
+{
+if(index==nums.size())
+{
+ans.push_back(nums);
+}
+
+  for(int i=0;i<nums.size();i++)
+    {
+
+if(i!=index 
+    }
+
+}
+
+};
+
