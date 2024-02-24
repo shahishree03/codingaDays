5ebcc66e4a6f0be385f4083ce649e361ad1a5a44
Shahi Shreshth <54995727+shahishree03@users.noreply.github.com>
2024-02-24 14:30:53 +0530

    Create rotate_Array_by_k_pos.cpp

---
+++ Day30/rotate_Array_by_k_pos.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#incldue<vector>
+using namespace std;
+void rotateArray(vector<int& arr, int n , int k )
+{
+if(k==0) return;
+
+int temp=arr[n-1];
+  for(int i=n-1;i>=0;i--)
+    {
+arr[i]=arr[i-1];
+    }
+  arr[0]=temp;
+
+}
+int main()
+{
+vector<int> arr={1,2,3,4,5,6,7};
+  rotateArray(arr, arr.size());
+  for(int i=0;i<arr.size();i++)
+{
+cout<<arr[i]<<"  "; 
+}
