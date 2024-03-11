629fadaeb3dd3e8e3f2676091db57165e7f9be5f
Shahi Shreshth <54995727+shahishree03@users.noreply.github.com>
2024-03-11 23:42:03 +0530

    Create flatten.cpp

---
+++ Day38/flatten.cpp
@@ -0,0 +1,23 @@
+Node* merge(Node* a, Node* b)
+{
+if(!a) return b;
+if(!b) return a;
+Node* res;
+  if(a->data<b->data)
+    res=a;
+    res->bottom =merge(a->bottom, b);
+  else
+  res=b;
+    res->bottom =merge(a, b->bottom);
+
+  return res;
+
+}
+
+Node* flatten(Node* root)
+{
+if(root==nullptr||root->next!=nullptr) return root;
+root->next=flatten(root->next);
+root=merge(root, root->next);
+return root;
+}
