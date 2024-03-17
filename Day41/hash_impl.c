64395ea8ad2d80925927080cf2509d78f75baae3
Shahi Shreshth <54995727+shahishree03@users.noreply.github.com>
2024-03-17 13:25:37 +0530

    Update hash_impl.c

--- Day41/hash_impl.c
+++ Day41/hash_impl.c
@@ -13,6 +13,30 @@ int hashcode(int key) {
     return key % 10;
 }
 
+void insert_with_freq(int key)
+{
+    Hash *t =(Hash*)malloc(sizeof(Hash));
+    t->key = key;
+   int hashIndex = hashcode(key);
+    for(int i=0;i<10;i++)
+    {
+    int hashIndex = hashcode(key);
+    while (arr[hashIndex] != NULL) {
+        if (arr[hashIndex]->key == key) {
+            // Key already exists, increment its frequency
+            (arr[hashIndex]->value)++;
+            return;
+        }
+        hashIndex++;
+        hashIndex %= 10;
+    }
+    // Key doesn't exist, insert a new element with frequency 1
+   
+     
+    }
+  
+     insert(key, 1);
+}
 void insert(int key, int value) {
     Hash *t = (Hash*)malloc(sizeof(Hash));
     t->key = key;
@@ -86,6 +110,23 @@ int main() {
             printf("NULL here\n");
         }
     }
+    
+     insert_with_freq(10);
+       insert_with_freq(10);
+         insert_with_freq(20);
+           insert_with_freq(5);
+             insert_with_freq(10);
+    
+    
+    printf("Hash table contents after Insert with frequnecxy :\n");
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] != NULL) {
+            printf("(%d %d)\n", arr[i]->key, arr[i]->value);
+        } else {
+            printf("NULL here\n");
+        }https://www.onlinegdb.com/online_c_compiler#tab-stdin
+    }
+    
 
     return 0;
 }
